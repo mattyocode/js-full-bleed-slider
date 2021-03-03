9cdcd9e825bebb1c0230d9d81a75d118850ce6c1
mattyocode <matthewoliver@live.co.uk>
2021-03-03 16:30:15 +0000

    refactor: counterInRange logic in function

--- app.js
+++ app.js
@@ -1,48 +1,57 @@
-const carouselSlide = document.querySelector('.carousel-slide');
-const carouselContent = document.querySelectorAll('.full-bleed');
+function responsiveCarousel(){
 
-// Buttons
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+    const carouselSlide = document.querySelector('.carousel-slide');
+    const carouselContent = document.querySelectorAll('.full-bleed');
 
-// Counter
-let counter = 0;
-const size = carouselContent[0].clientWidth;
-console.log(counter);
+    // Buttons
+    const prevBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
 
-carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    // Counter
+    let counter = 0;
+    const size = carouselContent[0].clientWidth;
+    console.log(counter);
 
-// Button listeners
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
-function checkBtnDisplay(){
-    if(counter <= 0 || counter >= carouselContent.length) {
-        prevBtn.classList.add('hide');
-    } else {
-        prevBtn.classList.remove('hide');
+    // Button listeners
+
+    function counterInRange(){
+        return (counter <= 0 || counter >= carouselContent.length);
     };
+
+    function checkBtnDisplay(){
+        if(counterInRange()) {
+            prevBtn.classList.add('hide');
+        } else {
+            prevBtn.classList.remove('hide');
+        };
+    };
+
+    nextBtn.addEventListener('click', () => {
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter++;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        checkBtnDisplay();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if(counter <= 0) {
+            return;
+        } else {
+        carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+        counter--;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+        checkBtnDisplay();
+    }});
+
+    window.addEventListener('DOMContentLoaded', () => {
+        checkBtnDisplay();
+    });
+
+    carouselSlide.addEventListener('transitionend', () => {
+        console.log('fired')
+    });
 };
 
-nextBtn.addEventListener('click', () => {
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    checkBtnDisplay();
-});
-
-prevBtn.addEventListener('click', () => {
-    if(counter <= 0) {
-        return;
-    } else {
-    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-    checkBtnDisplay();
-}});
-
-window.addEventListener('DOMContentLoaded', () => {
-    checkBtnDisplay();
-});
-
-carouselSlide.addEventListener('transitionend', () => {
-    console.log('fired')
-});
+responsiveCarousel();
